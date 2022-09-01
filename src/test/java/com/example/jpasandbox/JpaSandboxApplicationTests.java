1c78fb14ceb8bdc964ac75248daed6d0b26d2e94
bomi <bonita9931@gmail.com>
2022-09-01 09:34:29 +0900

    init project

---
+++ src/test/java/com/example/jpasandbox/JpaSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpasandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
