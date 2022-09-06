3506cd6c28ff3f09dfdcd228641603cf75ede920
bomi <bonita9931@gmail.com>
2022-09-06 09:36:03 +0900

    test: 1차 캐시에서 연관관계 조회시

---
+++ src/main/java/com/example/jpasandbox/domain/EagerMember.java
@@ -0,0 +1,30 @@
+package com.example.jpasandbox.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "eager_member")
+public class EagerMember {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    protected EagerMember() {
+    }
+
+    public EagerMember(final Long id, final String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public EagerMember(final String name) {
+        this.name = name;
+    }
+}
